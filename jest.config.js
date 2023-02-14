e0cee75124d9760d6328a652a6d8a1b58ce1ee01
Gérard Collin <gerard.collin@appliweb.net>
2023-02-14 18:43:28 +0100

    Increase test timeout

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  testTimeout: 15000,
   testEnvironment: "./tests/testEnvironment.js",
   globalSetup: "./tests/globalSetup.js",
   globalTeardown: "./tests/globalTeardown.js"
